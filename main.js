7441a6bfb5c2b8e2aa167a40c6174dd85667cef5
Eduardo Guette <eduardoguette@github.com>
2020-06-24 16:49:05 +0200

    Add vue

--- main.js
+++ main.js
@@ -3,26 +3,33 @@ if (window.matchMedia("(prefers-color-sheme: dark)").matches == true) {
   checkbox.setAttribute("checked", true);
 }
 if(localStorage.getItem("dark")){
-  document.querySelector('.dark-mode-title').textContent = "Dark Mode"
+  // document.querySelector('.dark-mode-title').textContent = "Dark Mode"
   document.body.classList.add("is-dark-mode");
   document.querySelector('.checkbox').checked = true
 }else{
   document.querySelector('.checkbox').checked = false
-  document.querySelector('.dark-mode-title').textContent = "Light Mode"
-
+  // document.querySelector('.dark-mode-title').textContent = "Light Mode"
 }
 checkbox.addEventListener("change", function () {
   
   if(this.checked && !localStorage.getItem("dark")){
     localStorage.setItem("dark","yes")
+    app.message = "Ligth Mode"
     document.body.classList.remove("is-light-mode");
     document.body.classList.add("is-dark-mode");
 
   }else{
     localStorage.removeItem("dark")
+    app.message = "Dark Mode"
     document.body.classList.remove("is-dark-mode");
     document.body.classList.add("is-light-mode");
   }
 });
 
 
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Dark Mode'
+  }
+})
